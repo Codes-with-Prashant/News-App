3c597c1bb091e82942c6ac9c3c5a40e62e7e8e14
PRASHANT GOEL <61662599+prashantgoel6475@users.noreply.github.com>
2022-07-29 19:50:23 +0530

    initial commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import placeholder from './placeholder.png'
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
+        return (
+            <div className="my-3">
+                <div className="card" style={{borderRadius:"4%",backgroundColor:"#F2EBE9"}}>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img style={{borderRadius:"8%", padding:"0.2rem"}} src={!imageUrl ? placeholder : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body" >
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+     
+}
+
+export default NewsItem
